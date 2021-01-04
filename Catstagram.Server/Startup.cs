e23a605e8225a2d50e3018b7d432e034f322ef0f
Ivaylo Kostov <ikostov87@gmail.com>
2021-01-04 04:35:57 +0200

    Configuration of JWT Authentication. There is a lot of messy code in startup class, but soon it will be refactored.

--- Catstagram.Server/Startup.cs
+++ Catstagram.Server/Startup.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using Catstagram.Server.Data;
 using Catstagram.Server.Data.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -7,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Catstagram.Server
 {
@@ -20,13 +23,40 @@ namespace Catstagram.Server
         {
             services
                 .AddDbContext<CatstagramDbContext>(options => options
-                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                    .UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
 
             //services.AddDatabaseDeveloperPageExceptionFilter();
 
             services
                 .AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<CatstagramDbContext>();
+
+            var applicationSettingsConfiguration = this.Configuration.GetSection("ApplicationSettings");
+            services.Configure<ApplicationSettings>(applicationSettingsConfiguration);
+
+            var appSettings = applicationSettingsConfiguration.Get<ApplicationSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services
+                .AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+
             services.AddControllers();
         }
 
@@ -38,6 +68,11 @@ namespace Catstagram.Server
             }
 
             app.UseRouting();
+
+            app.UseCors(options => options
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowAnyOrigin());
 
             app.UseAuthentication();
             app.UseAuthorization();
