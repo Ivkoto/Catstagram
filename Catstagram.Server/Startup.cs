f126075e1fb212cbb676bb3e5a8a2b1fe7c30e96
Ivaylo Kostov <ikostov87@gmail.com>
2021-01-04 03:35:58 +0200

    First commit

---
+++ Catstagram.Server/Startup.cs
@@ -0,0 +1,51 @@
+using Catstagram.Server.Data;
+using Catstagram.Server.Data.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Catstagram.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => this.Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddDbContext<CatstagramDbContext>(options => options
+                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services
+                .AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<CatstagramDbContext>();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDatabaseErrorPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
